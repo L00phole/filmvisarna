6a52b03c24e5c6d89006b6aedfd068f52381339f
GHHag <gustav.hagenblad@gmail.com>
2021-11-23 11:55:30 +0100

    added show_schedule.js, elements in index.html and some styling in style.css

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -10,7 +10,7 @@ async function readJSON() {
   }
 
   // Run start function
-  start();
+  createShowSchedule(4);
 }
 
 // A helper to find things by id (films, shows etc.)
@@ -18,10 +18,47 @@ function findById(type, id) {
   return data[type].find(x => x.id === id);
 }
 
-async function start() {
-  // console.log(findById('auditoriums', 2));
-  //console.log(await book(1, [30, 80, 81]));
-  console.log(freeSeats(1));
+function selectedShow(show) {
+  console.log("Gå till salong för visningen: ");
+  console.log(show);
 }
 
+/*async function start() {
+  // Calls function to create a show schedule for n(4) given weeks.
+  createShowSchedule(4);
+  //console.log(findById('auditoriums', 2));
+  //console.log(await book(1, [30, 80, 81]));
+  console.log(freeSeats(1));
+  console.log(freeSeats(2));
+  console.log(freeSeats(3));
+  console.log(freeSeats(4));
+}*/
+
 readJSON();
+
+/*
+Exempel
+*/
+
+// Läser filmer från json
+$('#movies').click(function () {
+  console.log(data['films']);
+  for (let i = 0; i < data['films'].length; i++) {
+    let container = document.getElementById("movies");
+
+    let movie = document.createElement('div');
+    movie.innerHTML = data['films'][i].title;
+    movie.addEventListener('click', function () {
+      console.log(JSON.stringify(data['films'][i]));
+      let movieInfo = document.createElement('p');
+      movieInfo.innerHTML = JSON.stringify(data['films'][i])
+      movie.appendChild(movieInfo);
+    });
+
+    container.appendChild(movie);
+  }
+});
+
+$('#tickets').click(function () {
+  console.log(freeSeats(movie));
+});
