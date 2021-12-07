335852f0e6d588d275b54617f2f0b7c30942ab2b
GHHag <gustav.hagenblad@gmail.com>
2021-12-07 15:05:15 +0100

    Co-authored-by: MergimJakupi <MergimJakupi@users.noreply.github.com> Co-authored-by: L00phole <L00phole@users.noreply.github.com>

--- frontend/js/show.js
+++ frontend/js/show.js
@@ -1,25 +1,126 @@
+// declare variables to hold data for ticket counts
+let ordTickets = 0;
+let seniorTickets = 0;
+let childTickets = 0;
+
+let availableSeatsCount = 0;
+
 // Adds components to show data for the show with the given id
 async function selectedShow(showId) {
-    showData = await JSON._load('shows.json');
-    let showInfoDiv = document.getElementById('show');
-    let showInfo = showData.find(show => show.id == showId);
+    // Calls function to get data of the selected show.
+    let showInfo = await getSelectedShowData(showId);
+    let showInfoDiv = document.getElementById('showInfo');
 
+    // Reads JSON file with auditorium data, gets the total number of
+    // seats for the auditorium of the selected show. 
+    let auditoriumData = await JSON._load('auditoriums.json');
+    let auditoriumTotalSeats = auditoriumData.find(aud => aud.name == showInfo.auditorium).seats;
+
+    // Creates components to display info about the selected show
     let film = document.createElement('p')
     film.innerHTML = showInfo.film;
+    film.style.fontSize = "40px";
     showInfoDiv.appendChild(film);
 
     let auditorium = document.createElement('p')
     auditorium.innerHTML = showInfo.auditorium;
     showInfoDiv.appendChild(auditorium);
 
-    let date = document.createElement('p')
-    date.innerHTML = showInfo.date;
-    showInfoDiv.appendChild(date);
+    let dateAndTime = document.createElement('p')
+    dateAndTime.innerHTML = showInfo.date + ", " + showInfo.time;
+    showInfoDiv.appendChild(dateAndTime);
 
-    let time = document.createElement('p')
-    time.innerHTML = showInfo.time;
-    showInfoDiv.appendChild(time);
+    // Gets the count of booked seats, and calculates the number of
+    // available seats. Creates a component to display it.
+    let bookedSeatsCount = await getSelectedShowBookedSeats(showId);
+    availableSeatsCount = auditoriumTotalSeats - bookedSeatsCount.length;
+    let availableSeats = document.createElement('p');
+    availableSeats.innerHTML = `Antal lediga platser: ${availableSeatsCount}`;
+    showInfoDiv.appendChild(availableSeats);
+
+    showInfoDiv.style.fontSize = "25px";
 }
+
+// Returns data from the show with the given id
+async function getSelectedShowData(showId) {
+    showData = await JSON._load('shows.json');
+    let showInfo = showData.find(show => show.id == showId);
+
+    return showInfo;
+}
+
+// Returns an array with the booked seats for the show with the given show id
+async function getSelectedShowBookedSeats(showId) {
+    bookings = await JSON._load('bookings.json');
+    let bookingsInfo = bookings.filter(booking => booking.showId == showId);
+
+    let bookedSeats = [];
+    for (let bookingInfo of bookingsInfo) {
+        bookedSeats = bookedSeats.concat([...bookingInfo.seats]);
+    }
+
+    return bookedSeats;
+}
+
+// Add click event to the component with the given id with functionality
+// to keep track of the count of bookings for each category.
+$('#ordCategorySub').click(() => {
+    if (ordTickets != 0) {
+        ordTickets -= 1;
+    }
+
+    document.getElementById('ordCategoryCount').innerHTML = ordTickets;
+});
+
+// Add click event to the component with the given id with functionality
+// to keep track of the count of bookings for each category.
+$('#ordCategoryAdd').click(() => {
+    if (ordTickets < availableSeatsCount) {
+        ordTickets += 1;
+    }
+
+    document.getElementById('ordCategoryCount').innerHTML = ordTickets;
+});
+
+// Add click event to the component with the given id with functionality
+// to keep track of the count of bookings for each category.
+$('#seniorCategorySub').click(() => {
+    if (seniorTickets != 0) {
+        seniorTickets -= 1;
+    }
+
+    document.getElementById('seniorCategoryCount').innerHTML = seniorTickets;
+});
+
+// Add click event to the component with the given id with functionality
+// to keep track of the count of bookings for each category.
+$('#seniorCategoryAdd').click(() => {
+    if (seniorTickets < availableSeatsCount) {
+        seniorTickets += 1;
+    }
+
+    document.getElementById('seniorCategoryCount').innerHTML = seniorTickets;
+});
+
+// Add click event to the component with the given id with functionality
+// to keep track of the count of bookings for each category.
+$('#childCategorySub').click(() => {
+    if (childTickets != 0) {
+        childTickets -= 1;
+    }
+
+    document.getElementById('childCategoryCount').innerHTML = childTickets;
+});
+
+// Add click event to the component with the given id with functionality
+// to keep track of the count of bookings for each category.
+$('#childCategoryAdd').click(() => {
+    if (childTickets < availableSeatsCount) {
+        childTickets += 1;
+    }
+
+    document.getElementById('childCategoryCount').innerHTML = childTickets;
+});
 
 // Reads the show id from URLSearchParams and passes it with a call
 // to the selectedShow function.
