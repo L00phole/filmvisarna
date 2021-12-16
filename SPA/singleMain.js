fce14ec5db687784ff2b2c343948be9070d6f214
adamLundstedt <adamlundstedt@rocketmail.com>
2021-12-16 08:28:36 +0100

    små fix

--- SPA/singleMain.js
+++ SPA/singleMain.js
@@ -18,7 +18,7 @@ function reactOnHashChange() {
 }
 
 
-let films;
+let films, shows, auditoriums, bookings;
 async function readJson() {
   let rawData = await fetch('/SPA/movies.json')
   films = await rawData.json();
@@ -27,7 +27,7 @@ async function readJson() {
 // Start page
 function start() {
   $('main').html(` 
-
+    <h1>Här ska första sidan vara.</h1>
   `);
 }
 
@@ -72,6 +72,11 @@ function aboutus() {
   `);
 }
 
+function bottom() {
+  $('main').html(` 
+    <h1>TEST TEST</h1>
+  `);
+}
 
 
 // Show info about a film
@@ -92,7 +97,7 @@ function film(id) {
     <div id="content">
       <h3><b>${film.title}</B></h3><br>
       <p><b>År: </b>${film.productionYear}</p><br>
-      <p><b>Speltid: </b>${film.length}</p><br>
+      <p><b>Speltid: </b>${film.length} min.</p><br>
       <p><b>Genre: </b>${film.genre}</p><br>
       <p><b>Utgivare: </b>${film.distributor}</p><br>
       <p><b>Språk: </b>${film.language}</p><br>
@@ -102,17 +107,17 @@ function film(id) {
       <p><b>Handling: </b>${film.description}</p><br>
       <p><b>Recensioner: </b></p>
       <p><b><em>${film.reviews[0].source}</em></b></p>
-      <p></p>
+      <p><em>${film.reviews[0].quote}</em></p>
       <p>Betyg: ${film.reviews[0].stars}/5</p>
-      <p><b><a href="${film.reviews[0].sourceLink}">Läs hela recensionen här.</a></b></p><br>
+      <p><b><a href="${film.reviews[0].sourceLink}"><u>Läs hela recensionen här.</u></a></b></p><br>
       <p><b><em>${film.reviews[1].source}</em></b></p>
-      <p></p>
+      <p><em>${film.reviews[1].quote}</em></p>
       <p>Betyg: ${film.reviews[1].stars}/5</p>
-      <p><b><a href="${film.reviews[1].sourceLink}">Läs hela recensionen här.</a></b></p><br>
+      <p><b><a href="${film.reviews[1].sourceLink}"><u>Läs hela recensionen här.</u></a></b></p><br>
       <p><b><em>${film.reviews[2].source}</em></b></p>
-      <p></p>
+      <p><em>${film.reviews[2].quote}</em></p>
       <p>Betyg: ${film.reviews[2].stars}/5</p>
-      <p><b><a href="${film.reviews[2].sourceLink}">Läs hela recensionen här.</a></b></p><br>
+      <p><b><a href="${film.reviews[2].sourceLink}"><u>Läs hela recensionen här.</u></a></b></p><br>
     </div>
   </div>
   <div class="container__content dropdown">
@@ -125,15 +130,76 @@ function film(id) {
       <div id="filmSchedule"></div>
 
     </div>
-    </div>
+    
   </div>
 
   `);
 }
-
-
-
 // Listen to the hash change event
 window.onhashchange = reactOnHashChange;
 // And also call reactOnHashChange on initial page load
 reactOnHashChange();
+//From movie.js
+let data = {};
+
+async function readJSON(filmTitle) {
+  // Read json and put in the data variable
+  for (let type of ['shows', 'auditoriums', 'bookings']) {
+    data[type] = await JSON._load('frontend/json' + type + '.json');
+  }
+
+  populateFilmShowSchedule(filmTitle);
+}
+
+// Populates a component with a filmschedule for the given title.
+function populateFilmShowSchedule(filmTitle) {
+  let container = document.getElementById('filmSchedule');
+
+  let showsH = document.createElement('h1');
+  showsH.innerHTML = "Visningar";
+  container.append(showsH);
+
+  let filmSchedule = document.createElement('div');
+  filmSchedule.setAttribute('class', 'filmScheduleDiv');
+  container.appendChild(filmSchedule);
+
+  // Iterate over the shows
+  for (let show of data['shows']) {
+    // If show['film'] is the same as the given argument for filmTitle
+    // create elements for displaying data for the show.
+    if (show['film'] == filmTitle) {
+      // Creates div component to display show data in, and sets attributes
+      // for class and id
+      let showDataDiv = document.createElement('div');
+      showDataDiv.setAttribute('class', 'showDataDiv')
+      showDataDiv.setAttribute('id', 'showDataDiv_' + show.id);
+
+      let date = document.createElement('span');
+      date.innerHTML = show['date'];
+      date.setAttribute('class', 'filmScheduleSpan');
+      let time = document.createElement('span')
+      time.innerHTML = show['time'];
+      time.setAttribute('class', 'filmScheduleSpan');
+      date.appendChild(time);
+      let auditorium = document.createElement('span')
+      auditorium.innerHTML = show['auditorium']
+      auditorium.setAttribute('class', 'filmScheduleSpan');
+
+      showDataDiv.appendChild(date);
+      showDataDiv.appendChild(auditorium);
+      showDataDiv.style.backgroundColor = "darkgrey";
+
+      filmSchedule.appendChild(showDataDiv);
+
+      // Add click event to to showDataDiv, the event redirects to the page
+      // for the selected show.
+      $('#showDataDiv_' + show.id).on("click", function () {
+        window.location.assign(`${window.location['origin']}/show.html?id=${show.id}`);
+      });
+    }
+  }
+
+  container.style.alignContent = "center";
+  container.style.textAlign = "center";
+}
+
