7f3540424431f6e11ea3dd47f4a42af48ae4fd33
MergimJakupi <baka.shiro20@gmail.com>
2021-12-20 23:37:10 -0800

    changed index.js to node, added spa line comment

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ const express = require('express');
 const app = express();
 
 // Adding jsonflex and telling the  webserver to use jsonflex
-const jsonflex = require('jsonflex')({ jsonDir: '/frontend_spa/json' });
+const jsonflex = require('jsonflex')({ jsonDir: '/frontend/json' });
+//const jsonflex = require('jsonflex')({ jsonDir: '/frontend_spa/json' });
 app.use(jsonflex);
 
 // Tell the web server to serve files from the frontend folder
-app.use(express.static('frontend_spa'));
+app.use(express.static('frontend'));
+//app.use(express.static('frontend_spa'));
 
 // Start the web server at port 3000
 app.listen(3000, () => console.log('Listening on port 3000'));
